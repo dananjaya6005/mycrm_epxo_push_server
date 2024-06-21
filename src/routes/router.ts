51e184110bbbc9327e941f424c40d383ee1abe53
dananjaya6005 <dananjaya6005@gmail.com>
2024-06-22 01:48:08 +0530

    '3'

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,12 @@
-import express from 'express';
+import express, { response } from 'express';
 import axios from 'axios';
+import { createClient } from '@supabase/supabase-js'
+
 
 const router =  express.Router();
 const url = 'https://exp.host/--/api/v2/push/send';
 
+const supabase = createClient('https://yktmwnnuxbftenvcwnll.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrdG13bm51eGJmdGVudmN3bmxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg5NzcyOTQsImV4cCI6MjAzNDU1MzI5NH0.wy-1sdekjnhJ9Gb-UYpsJM3lJpzvg5Y-E7VFvGAXg8c');
 
 
 router.get('/api/cron-job', (req, res) => {
@@ -13,44 +16,64 @@ router.get('/api/cron-job', (req, res) => {
     });
 });
 
-router.post('/v1/push', (req, res) => {
+router.post('/v1/push', async (req, res) => {
 
-    const {username} = req.body;
-    const {expo_token} = req.body;
+    const { title, body,  } = req.body;
+    const usernames = req.body.users.split(',');
 
-    console.log('req.body', req.body);
+    console.log('usernames', usernames );
     
 
-    // console.log('username', username);
-    // console.log('expo_token', expo_token);
+    console.log('req.body', req.body);
 
-    if (!username || !expo_token) {
+
+    if (!title || !body || usernames.length === 0) {
         return res.status(400).json({
             success: false,
-            message: 'Username and expo_token are required',
+            message: 'Check fileds again or  at least one user is required',
         });
+    } else {
+        //get tokens from supabase
+
+       const { data, error } = await supabase.from('mycrm_expo_push_token').select('expo_token').in('user_name', usernames);
+         console.log(data);
+
+
+
+         data?.forEach(async (element) => {
+
+            const expo_token = element.expo_token;
+
+            const dataforNotification = {
+                to: expo_token,
+                title: title,
+                body: body,
+              };
+
+
+
+                    
+            await axios.post(url, dataforNotification, {
+                headers: {
+                    Accept: 'application/json',
+                    'Accept-encoding': 'gzip, deflate',
+                    'Content-Type': 'application/json',
+                },
+            }).then((response) => {
+                console.log('response', response.data);
+                }
+            ).catch((error) => {
+                console.log('error', error);
+            }
+        );
+    
+
+         })
+
+
+
     }
-    //sent to expo offcial server
-
-    const data = {
-        to: expo_token,
-        title: 'hello 2 world',
-        body: 'world',
-      };
-
-    axios.post(url, data, {
-        headers: {
-            Accept: 'application/json',
-            'Accept-encoding': 'gzip, deflate',
-            'Content-Type': 'application/json',
-        },
-    }).then((response) => {
-        console.log('response', response.data);
-        }
-    ).catch((error) => {
-        console.log('error', error);
-     }
-    );
+    
 
 
     res.status(200).json({
